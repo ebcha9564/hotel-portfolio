e61d66bb927c241490a7ffa27cdb12204a9e65b5
Eunbin Cha <eric9564@gmail.com>
2025-05-16 18:19:40 +0900

    sdasa
    asdsasadsda

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,66 @@
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'right',
+    drops: 'up'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
+
+
 var swiper = new Swiper(".mySwiper-0", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
-    effect: 'fade', 
+    effect: 'fade',
     fadeEffect: {
-      crossFade: true 
+        crossFade: true
     },
-    speed: 2000, 
+    speed: 2000,
     autoplay: {
-        delay: 2500, 
-        disableOnInteraction: false, 
+        delay: 2500,
+        disableOnInteraction: false,
         pauseOnMouseEnter: true
-      },
-    pagination: {
-      el: ".swiper-pagination-0",
-      clickable: true,
     },
-  });
+    pagination: {
+        el: ".swiper-pagination-0",
+        clickable: true,
+    },
+});
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const plusButtons = document.querySelectorAll('.plus');
+    const minusButtons = document.querySelectorAll('.minus');
+
+    plusButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const parentDiv = this.parentElement;
+            const numberElement = parentDiv.querySelector('p');
+            let currentValue = parseInt(numberElement.textContent);
+
+            if (currentValue < 5) {
+                numberElement.textContent = currentValue + 1;
+            } else {
+                alert('Rooms can be only reserved up to 5 rooms.');
+            }
+        });
+    });
+
+    minusButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const parentDiv = this.parentElement;
+            const numberElement = parentDiv.querySelector('p');
+            let currentValue = parseInt(numberElement.textContent);
+
+            if (currentValue > 0) {
+                numberElement.textContent = currentValue - 1;
+            } else {
+                alert('Number cannot be negative.');
+            }
+        });
+    });
+});
 
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
@@ -30,7 +73,7 @@ var swiper = new Swiper(".mySwiper", {
     },
     on: {
         slideChange: function () {
-            const texts = document.querySelectorAll('.slide-text');
+            const texts = document.querySelectorAll('.slide-text-6');
 
             texts.forEach(text => {
                 text.style.display = "none";
@@ -46,7 +89,7 @@ var swiper = new Swiper(".mySwiper", {
         },
 
         init: function () {
-            const firstText = document.querySelector('.slide-text');
+            const firstText = document.querySelector('.slide-text-6');
             firstText.style.display = "block";
             firstText.classList.add('active');
         }
@@ -63,7 +106,7 @@ var swiper = new Swiper(".mySwiper-1", {
     },
     on: {
         init: function () {
-            const textSlides = document.querySelectorAll('.slide-text-1');
+            const textSlides = document.querySelectorAll('.slide-text-5');
             textSlides.forEach((slide, index) => {
                 if (index === 0) {
                     slide.classList.add('active');
@@ -75,7 +118,7 @@ var swiper = new Swiper(".mySwiper-1", {
         slideChange: function () {
             const activeIndex = this.activeIndex;
 
-            const textSlides = document.querySelectorAll('.slide-text-1');
+            const textSlides = document.querySelectorAll('.slide-text-5');
 
             textSlides.forEach(slide => {
                 slide.classList.remove('active');
@@ -90,33 +133,32 @@ var swiper = new Swiper(".mySwiper-1", {
 
 
 var swiper = new Swiper(".mySwiper-2", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    speed: 300,
-    pagination: {
-      el: ".swiper-pagination-2",
-      clickable: true,
-    },
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: ".next-1",
+        prevEl: ".prev-1",
+      },
 
     breakpoints: {
 
-        1920:{
+        1920: {
             slidesPerView: 3,
             spaceBetween: 30,
         },
 
-        768:{
+        768: {
             slidesPerView: 2,
             spaceBetween: 20
         },
 
-        540:{
+        540: {
             slidesPerView: 1,
             spaceBetween: 10
         },
-        
-        348:{
+
+        348: {
             slidesPerView: 1,
             spaceBetween: 5,
         }
@@ -125,20 +167,20 @@ var swiper = new Swiper(".mySwiper-2", {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const roomTabs = document.getElementById('roomTabs');
     const section02 = document.getElementById('sec02-accomandations');
     const tabPanes = document.querySelectorAll('.tab-pane');
-    
-    roomTabs.addEventListener('click', function(e) {
+
+    roomTabs.addEventListener('click', function (e) {
         if (e.target.classList.contains('nav-link')) {
             tabPanes.forEach(pane => {
                 pane.classList.remove('active');
                 pane.classList.remove('show');
             });
-            
+
             section02.classList.remove('bg-standard', 'bg-premier', 'bg-family', 'bg-king');
-            
+
             setTimeout(() => {
                 if (e.target.id === 'standard-tab') {
                     section02.classList.add('bg-standard');
@@ -153,7 +195,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     section02.classList.add('bg-king');
                     document.getElementById('king').classList.add('active', 'show');
                 }
-            }, 0); 
+            }, 0);
         }
     });
 });
