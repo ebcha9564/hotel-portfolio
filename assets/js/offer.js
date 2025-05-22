53d7d908cc7296f1beb90e538125de6400f315a0
Eunbin Cha <eric9564@gmail.com>
2025-05-22 18:22:06 +0900

    special offer
    special offer

---
+++ assets/js/offer.js
@@ -0,0 +1,12 @@
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
